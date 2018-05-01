3f229c0a5ff6d15052356347518831bc04d9a1de
Tudor Croitoru <ihorvalds@outlook.com>
2018-05-02 00:20:24 +0300

    Small fix

--- brethren/settings.py
+++ brethren/settings.py
@@ -47,7 +47,7 @@ REST_FRAMEWORK = {
     # 'DEFAULT_AUTHENTICATION_CLASSES': (
     #     'rest_framework.authentication.BasicAuthentication',
     #     'rest_framework.authentication.SessionAuthentication',
-    # )
+    # )ange
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
